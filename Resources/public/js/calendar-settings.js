110794af1e2f1cce12d9bb014f4cbdf03c10ac6d
Rob T <rob.tromans@tec20.co.uk>
2016-12-13 13:29:19 +0000

    Add remove event dialog.
    Signed-off-by: Rob T <rob.tromans@tec20.co.uk>

--- Resources/public/js/calendar-settings.js
+++ Resources/public/js/calendar-settings.js
@@ -3,70 +3,73 @@ $(function() {
 	var d = date.getDate();
 	var m = date.getMonth();
 	var y = date.getFullYear();
+	
 
 	$('#calendar-holder').fullCalendar(
-			{
-				header : {
-					left : 'prev, next',
-					center : 'title',
-					right : 'month,agendaWeek,agendaDay,'
-				},
-				allDaySlot : false,
-				dayClick : function(date, jsEvent, view) {
-					$('#calendar-holder').fullCalendar('changeView', 'agendaDay');
-					$('#calendar-holder').fullCalendar('gotoDate', date);
-				},
-				droppable : true,
-				drop : function(date, allDay, jsEvent, ui) {
-					if($('#calendar-holder').fullCalendar('getView').name == 'month') {
-						if(date.getHours() == 0) {
-							date.setHours(8);
-						}
+		{
+			header : {
+				left : 'prev, next',
+				center : 'title',
+				right : 'month,agendaWeek,agendaDay,'
+			},
+			allDaySlot : false,
+			dayClick : function(date, jsEvent, view) {
+				$('#calendar-holder').fullCalendar('changeView', 'agendaDay');
+				$('#calendar-holder').fullCalendar('gotoDate', date);
+			},
+			droppable : true,
+			drop : function(date, allDay, jsEvent, ui) {
+				if($('#calendar-holder').fullCalendar('getView').name == 'month') {
+					if(date.getHours() == 0) {
+						date.setHours(8);
 					}
-					var element = this;
-					$.ajax({
-						url : Routing.generate('fullcalendar_event_dropped'),
-						data : {
-							date : date,
-							id : this.id,
-							installationId: $(this).prop('installationId')
-						},
-						success : function(data, textStatus, jqXHR) {
-							$('#calendar-holder').fullCalendar('renderEvent',
-									data, true);
-							$(element).remove();
-						}
-					});
-				},
-				firstDay : 1,
-				lazyFetching : true,
-				weekNumbers : true,
-				theme : false,
-				timeFormat : {
-					// for agendaWeek and agendaDay
-					agenda : 'hh:mm', // 5:00 - 6:30
-
-					// for all other views
-					'' : 'hh:mm' // 7p
-				},
-				eventSources : [ {
-					url : Routing.generate('fullcalendar_loader'),
-					type : 'POST',
-					error : function() {
-						// alert('There was an error while fetching Google
-						// Calendar!');
-					}
-				} ],
-				eventClick : function(event, element) {
-					console.log('click');
-				},
-				editable : true,
-				eventDrop : function(event, dayDelta, minuteDelta, allDay,
-						revertFunc, jsEvent, ui, view) {
-					$.ajax({
-						url : Routing.generate('fullcalendar_event_dragged'),
-						data : event
-					});
 				}
-			});
+				var element = this;
+				$.ajax({
+					url : Routing.generate('fullcalendar_event_dropped'),
+					data : {
+						date : date,
+						id : this.id,
+						installationId: $(this).prop('installationId')
+					},
+					success : function(data, textStatus, jqXHR) {
+						$('#calendar-holder').fullCalendar('renderEvent',
+							data, true);
+						$(element).remove();
+					}
+				});
+			},
+			firstDay : 1,
+			lazyFetching : true,
+			weekNumbers : true,
+			theme : false,
+			timeFormat : {
+				// for agendaWeek and agendaDay
+				agenda : 'hh:mm', // 5:00 - 6:30
+
+				// for all other views
+				'' : 'hh:mm' // 7p
+			},
+			eventSources : [ {
+				url : Routing.generate('fullcalendar_loader'),
+				type : 'POST',
+				error : function() {
+				}
+			} ],
+			eventClick : function(event) {
+				$('#calendar-remove-modal #event_id').val(event.id);
+				$('#calendar-remove-modal').modal();
+			},
+			editable : true,
+			eventDrop : function(event, dayDelta, minuteDelta, allDay,
+								 revertFunc, jsEvent, ui, view) {
+				$.ajax({
+					url : Routing.generate('fullcalendar_event_dragged'),
+					data : event
+				});
+			},
+			eventRender: function (event, element) {
+				$("<i class=\"icon-remove-sign\" style=\"float: right\"></i>").insertBefore(element.find('.fc-event-title'));
+			}
+		});
 });
